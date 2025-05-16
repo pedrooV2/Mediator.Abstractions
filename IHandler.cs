a8a99ca12ef76ef782839303fa74b89d15ad3da8
Pedro Lucas <pedro.lucas210600@gmail.com>
2025-05-16 14:47:17 -0300

    Starting repo

---
+++ IHandler.cs
@@ -0,0 +1,6 @@
+namespace Mediator.Abstractions;
+
+public interface IHandler<in TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken = default);
+}
